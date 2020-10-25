a884109d9e65f2ff271369ef4e467645e2093d3b
mii-10 <miiakayou@gmail.com>
2020-10-25 21:17:14 +0900

    userinfoの不具合を修正

--- cogs/m10s_userinfo.py
+++ cogs/m10s_userinfo.py
@@ -19,14 +19,14 @@ class m10s_userinfo(commands.Cog):
             target = ctx.author
 
         headers ={
-            "Authorization":f"Authorization {self.bot.ydk_token}"
+            "Authorization":f"Bearer {self.bot.ydk_token}"
         }
         async with aiohttp.ClientSession() as session:
             async with session.get(f'https://api.yudzuki.pw/v1/users/{target.id}',headers=headers) as rs:
                 if rs.status == 200:
                     rtn = await rs.json()
                     if rtn["result"]:
-                        ydk_ev = rs.json()["settings"]["evaluate_value"]
+                        ydk_ev = rtn["settings"]["evaluate_value"]
                     else:
                         ydk_ev = -1
                 else:
@@ -38,9 +38,9 @@ class m10s_userinfo(commands.Cog):
         e.add_field(name="Discord Bot:結月による評価値",value=ydk_ev if ydk_ev != -1 else "評価値が取得できませんでした")
         e.add_field(name="役職リスト",value="\n".join([i.mention for i in target.roles]))
         if admin_role in ctx.author.roles:
-            e.set_footer(text=(target.created_at + rdelta(hours=9)).strftime('%Y{0}%m{1}%d{2} %H{3}%M{4}%S{5}').format(*'年月日時分秒'))
+            e.set_footer(text=(target.created_at + rdelta(hours=9)).strftime('%Y年%m月%d日%H時%M分%S秒'))
         if ctx.author.id == 525658651713601536:
-            e.add_field(name="サーバー内権限",value=f"```{','.join(target.guild_permissions)}```")
+            e.add_field(name="サーバー内権限",value=f"```{','.join([i[0] for i in iter(target.guild_permissions) if i[1]])}```")
         await ctx.send(embed=e)
         
 
