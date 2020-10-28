d36d2b02c244583921fab4d8b6431763913d1007
mii-10 <miiakayou@gmail.com>
2020-10-28 22:31:18 +0900

    update_user_badges

--- cogs/m10s_userinfo.py
+++ cogs/m10s_userinfo.py
@@ -31,8 +31,21 @@ class m10s_userinfo(commands.Cog):
                     ydk_ev = -1
         
         badges = ""
-        if target.id in self.bot.developers:
+        role_ids = [i.id for i in target.roles]
+        if 770996612503175258 in role_ids: #開発者チーム
             badges+="💠"
+        if 770996609533607958 in role_ids: #認証アカウント
+            badges+="✅"
+        if 599804873118187520 in role_ids: #Bot
+            badges+="⚙"
+        if 601052213435039745 in role_ids: #運営@サポート
+            badges+="🛠"
+        if 611730459402960907 in role_ids: #サーバーブースター
+            badges+="✨"
+        if 749245514788241479 in role_ids: #Vip
+            badges+="🎀"
+        if 676051183923757108 in role_ids: #SP-Vip
+            badges+="🎗"
 
         e=discord.Embed(color=self.bot.color)
         e.set_author(name=badges+str(target),icon_url=target.avatar_url_as(static_format="png"))
@@ -40,7 +53,7 @@ class m10s_userinfo(commands.Cog):
         e.add_field(name="Discord Bot:結月による評価値",value=ydk_ev if ydk_ev != -1 else "評価値が取得できませんでした")
         e.add_field(name="役職リスト",value="\n".join([i.mention for i in target.roles]))
         if admin_role in ctx.author.roles:
-            e.set_footer(text=f"{(target.created_at + rdelta(hours=9)).strftime('%Y年%m月%d日%H時%M分%S秒')}({(datetime.datetime.now()-(target.created_at+ rdelta(hours=9))).days}日前にアカウント作成")
+            e.set_footer(text=f"{(target.created_at + rdelta(hours=9)).strftime('%Y年%m月%d日%H時%M分%S秒')}({(datetime.datetime.now()-(target.created_at+ rdelta(hours=9))).days}日前にアカウント作成)")
         if ctx.author.id == 525658651713601536:
             e.add_field(name="サーバー内権限",value=f"```{','.join([i[0] for i in iter(target.guild_permissions) if i[1]])}```")
         await ctx.send(embed=e)
