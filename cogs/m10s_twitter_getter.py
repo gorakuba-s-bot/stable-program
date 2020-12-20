aa0a323ff9d2311177deb662cfe5973ff1203110
takumi0213 <52845601+takumi0213@users.noreply.github.com>
2020-12-20 23:05:48 +0900

    Twitter通知の設定

--- cogs/m10s_twitter_getter.py
+++ cogs/m10s_twitter_getter.py
@@ -11,23 +11,16 @@ import twitter
 
 
 
-class m10s_twinotif(commands.Cog):
+class m10s_twinotif_gorakuba(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         self.twi = twitter.Twitter(auth=twitter.OAuth(cf.T_Acs_Token, cf.T_Acs_SToken, cf.T_API_key, cf.T_API_SKey))
-        self.target = ["gorakubacrafter"]
+        self.target = ["inutanuking","39_encho","Pentonic005","3bis28","gorakubacrafter"]
         self.last_id = [self.gtwi_fu(i)[0] for i in self.target]
         self.ch = self.bot.get_channel(679652940742787114)
-        # ↓test
-        # self.ch = self.bot.get_channel(754636969698525235)
         self.loop_task.start()
 
-    @commands.command()
-    async def target_update(self,ctx,target_name):
-        if not ctx.author.id in [522788320879575050, 631819238662864897]: # めろんさんじゃないなら
-            self.target = target_name
-            await ctx.send(f"ターゲットを{target_name}に更新しました！")
 
     def gtwi_fu(self,uname):
         ret = self.twi.statuses.user_timeline(screen_name=uname,count=1)
@@ -40,7 +33,7 @@ class m10s_twinotif(commands.Cog):
             if not self.last_id[i] == ret[i][0]:
                 self.last_id = ret[i][0]
                 tweet = ret[i][1]
-                embed = discord.Embed(title="Twitter アップデート！",description=tweet["text"], color=int(
+                embed = discord.Embed(description=tweet["text"], color=int(
                     tweet["user"]["profile_background_color"], 16))
                 embed.set_author(name=f'{tweet["user"]["name"]}(@{tweet["user"]["screen_name"]})',
                                     url=f'https://twitter.com/{tweet["user"]["screen_name"]}', icon_url=tweet["user"]["profile_image_url_https"])
@@ -53,4 +46,4 @@ class m10s_twinotif(commands.Cog):
                 await self.ch.send(embed=embed)
 
 def setup(bot):
-    bot.add_cog(m10s_twinotif(bot))
+    bot.add_cog(m10s_twinotif_gorakuba(bot))
