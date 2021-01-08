4eb1611280df440dd15800bcd1b4dc0c6c6bb4a7
takumi0213 <52845601+takumi0213@users.noreply.github.com>
2021-01-08 23:56:40 +0900

    fix ui

--- cogs/m10s_userinfo.py
+++ cogs/m10s_userinfo.py
@@ -48,7 +48,6 @@ class m10s_userinfo(commands.Cog):
         e=discord.Embed(color=self.bot.color)
         e.set_author(name=badges+str(target),icon_url=target.avatar_url_as(static_format="png"))
         e.add_field(name="オンライン状況",value=target.status)
-        e.add_field(name="Discord Bot:結月による評価値",value=ydk_ev if ydk_ev != -1 else "評価値が取得できませんでした")
         e.add_field(name="役職リスト",value="\n".join([i.mention for i in target.roles]))
         if admin_role in ctx.author.roles:
             e.set_footer(text=f"{(target.created_at + rdelta(hours=9)).strftime('%Y年%m月%d日%H時%M分%S秒')}({(datetime.datetime.now()-(target.created_at+ rdelta(hours=9))).days}日前にアカウント作成)")
