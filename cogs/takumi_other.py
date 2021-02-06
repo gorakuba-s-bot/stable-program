972b0c1eaa64176612d23b7ed957ad557b1a78b2
takumi0213 <52845601+takumi0213@users.noreply.github.com>
2021-02-06 22:43:30 +0900

    add manage sys

--- cogs/takumi_other.py
+++ cogs/takumi_other.py
@@ -33,6 +33,58 @@ class other(commands.Cog):
             name="メモリ", value=f"全てのメモリ容量:{allmem}GB\n使用量:{used}GB({memparcent}%)\n空き容量{ava}GB({100-memparcent}%)")
         await ctx.send(embed=embed)
 
+    
+    @commands.group()
+    @commands.is_owner()
+    async def manage_features(self, ctx):
+        pass
+
+    @manage_features.command(name="view")
+    async def view_(self,ctx,uid:int):
+        await ctx.reply(f"```py\n{self.bot.features.get(uid,[])}```")
+
+    @manage_features.command(name="del")
+    async def del_(self,ctx,uid:int,feature):
+        uf = self.bot.features.get(uid,None)
+        if uf and feature in uf:
+            self.bot.features[uid].remove(feature)
+        await ctx.message.add_reaction(self.bot.get_emoji(790552892838248448))
+
+    @manage_features.command(name="add")
+    async def add_(self,ctx,uid:int,feature):
+        uf = self.bot.features.get(uid,None)
+        if uf:
+            self.bot.features[uid].append(feature)
+        else:
+            self.bot.features[uid] = [feature]
+        await ctx.message.add_reaction(self.bot.get_emoji(790552892838248448))
+
+    @manage_features.command(name="reload")
+    async def reload_(self,ctx):
+        import importlib
+        import config
+        importlib.reload(config)
+        self.bot.features = config.sp_features
+        await ctx.message.add_reaction(self.bot.get_emoji(790552892838248448))
+
+    @commands.command()
+    async def emojiinfo(self, ctx, *, emj: commands.EmojiConverter=None):
+
+        if emj is None:
+            await ctx.send(ctx._("einfo-needarg"))
+        else:
+            embed = discord.Embed(
+                title=emj.name, description=f"id:{emj.id}", color=self.bot.ec)
+            embed.add_field(name=ctx._("einfo-animated"), value=emj.animated)
+            embed.add_field(name=ctx._("einfo-manageout"), value=emj.managed)
+            if emj.user:
+                embed.add_field(name=ctx._("einfo-adduser"),
+                                value=str(emj.user))
+            embed.add_field(name="url", value=emj.url)
+            embed.set_footer(text=ctx._("einfo-addday"))
+            embed.set_thumbnail(url=emj.url)
+            embed.timestamp = emj.created_at
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(other(bot))
